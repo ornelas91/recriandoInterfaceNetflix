1a9f705e48926ec1792361b9a4c0fd15c7f8c862
ornelas91 <anderson_ornelas91@hotmail.com>
2022-01-31 20:12:19 -0300

    Atualização

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ $('.owl-carousel').owlCarousel({
     autoplayTimeout:4000,
     autoplayHoverPause:true,
     items:1,
+    nav:true,
     merge:true,
     loop:true,
     margin:10,
@@ -17,4 +18,21 @@ $('.owl-carousel').owlCarousel({
             items:4
         }
     }
-})
+});
+
+
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+
+
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
